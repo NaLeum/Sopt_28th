2c95d156b1341bcfbc606cc8e5e4577c866ca046
NaLeum <wognsdl@likelion.org>
2021-06-26 23:17:13 +0900

    creatPromiseThunk 함수 생성

--- thunk/src/lib/asyncUtils.js
+++ thunk/src/lib/asyncUtils.js
@@ -1,3 +1,26 @@
+export const createPromiseThunk = (type,promomisCreator) => {
+    const [SUCCESS,ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+    const thunkCreator = param = async dispatch => {
+        dispatch({type})
+        try{
+            const payload = await promomisCreator(param);
+            dispatch({
+                type: SUCCESS,
+                payload
+            })
+        }catch(e){
+            dispatch({
+                type: ERROR,
+                payload: e,
+                error: true
+            })
+        }
+    }
+
+    return thunkCreator;
+}
+
 export const reducerUtils = {
     initial : (data=null) => ({
         loading: false,
@@ -5,7 +28,7 @@ export const reducerUtils = {
         error: null
     }),
     // prevState 를 옵션으로 준이유: 지금은 loading 하면 데이터를 null로 바꾸지만, 상황에 따라 loading값만 바꿀떄도 있기 떄문
-    
+
     loading: (prevState = null) => ({
         data: prevState,
         loading: true,
