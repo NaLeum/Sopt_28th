9a29cfcda3553e4618a838a1835c30ac0b8dd3ae
NaLeum <wognsdl@likelion.org>
2021-06-26 23:28:10 +0900

    handleAsyncActions

--- thunk/src/lib/asyncUtils.js
+++ thunk/src/lib/asyncUtils.js
@@ -45,3 +45,29 @@ export const reducerUtils = {
         error
     })
 }
+
+export const handleAsyncActions = (type,key) => {
+    const [SUCCESS,ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+    const reducer = (state, action) => {
+        switch (action.type){
+            case type:
+                return{
+                    ...state,
+                    [key]: reducerUtils.loading()
+                }
+            case SUCCESS:
+               return { 
+                   ...state,
+                   [key]:reducerUtils.success(action.payload)
+                }
+            case ERROR:
+                return{
+                    ...state,
+                    [key]:reducerUtils.error(action.payload)
+                }
+            default:
+                return state;
+        }
+    }
+    return reducer;
+}
